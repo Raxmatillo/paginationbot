7e0b2fc89c3da73e947f54258e2d5d3febc1922f
raxmatillo <milodcomposer@gmail.com>
2023-02-26 14:48:10 +0500

    first_commit

---
+++ main.py
@@ -0,0 +1,53 @@
+from aiogram import Bot, Dispatcher, executor
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import StatesGroup, State
+from aiogram.types import Message, ParseMode
+
+from aiogram_dialog import Window, Dialog, DialogRegistry, DialogManager, StartMode
+from aiogram_dialog.widgets.kbd import Button
+from aiogram_dialog.widgets.text import Const
+from database.sqlite import Database
+
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+storage = MemoryStorage()
+bot = Bot(token=env.str("BOT_TOKEN"))
+dp = Dispatcher(bot, storage=storage)
+db = Database(path_to_db="main.db")
+registry = DialogRegistry(dp)
+
+
+class MySG(StatesGroup):
+    main = State()
+
+
+
+cars = db.select_all_cars()[:10]
+
+i = 1
+text = ""
+for car in cars:
+    text += f"<b>{i})</b> {car[1]} - {car[2]}\n"
+    i += 1
+
+main_window = Window(
+    Const(text),
+    Button(Const("Useless button"), id="nothing"),
+    parse_mode=ParseMode.HTML,
+    state=MySG.main,
+)
+dialog = Dialog(main_window)
+registry.register(dialog)
+
+
+@dp.message_handler(commands=["start"])
+async def start(m: Message, dialog_manager: DialogManager):
+    await dialog_manager.start(MySG.main, mode=StartMode.RESET_STACK)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
